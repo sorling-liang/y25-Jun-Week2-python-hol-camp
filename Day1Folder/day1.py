cfd478653308cab304a693248f6f5254bf81569a
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-09 02:06:13 +0000

    Jun 9, 2025, 2:06 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,6 +1,6 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+print("hello my name is Ms Liang")
 
 ########################################################################
 # Task 1:
