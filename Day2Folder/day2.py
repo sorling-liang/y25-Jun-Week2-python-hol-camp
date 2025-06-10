10b492c4caf77509c1ca1f772359d472ab4e847d
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-10 02:58:24 +0000

    Jun 10, 2025, 2:58 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -10,7 +10,7 @@ print("hello from day2")
 
 ########################################################################
 # Task 2:
-for apple in range(50):
+for apple in range(1, 50):
     print(apple)
 
 
