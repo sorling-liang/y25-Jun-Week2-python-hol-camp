c6be2b453846b9484108c32b7a047695f4c7379b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-10 01:21:09 +0000

    Jun 10, 2025, 1:21 AM

--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,19 @@
 
 
 ################## On Chat Commands Section #####################
+
+
+def teleport():
+    agent.teleport_to_player()
+
+player.on_chat("tp", teleport)
+
+def turnleft():
+    agent.turn(LEFT)
+
+player.on_chat("tl", turnleft)
+
+def turnright():
+    agent.turn(RIGHT)
+    
+player.on_chat("tr", turnright)
