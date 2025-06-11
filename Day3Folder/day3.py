d82fb8dc741db399def092a9f40ecb68549c835d
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-11 02:28:34 +0000

    Jun 11, 2025, 2:28 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -5,11 +5,11 @@ print("hello from day3")
 ########################################################################
 # Task 1:
 
-yourname = input("what is your name?")
-print("Hello " + yourname)
-yourage = input("how old are you?")
-yourage = int(yourage) + 3 # convert string to a number to do math operation
-print("I am " + str(yourage) + " this year.") # convert number to string to print out a sentence
+# yourname = input("what is your name?")
+# print("Hello " + yourname)
+# yourage = input("how old are you?")
+# yourage = int(yourage) + 3 # convert string to a number to do math operation
+# print("I am " + str(yourage) + " this year.") # convert number to string to print out a sentence
 
 
 yourname = input("what is your name?")
