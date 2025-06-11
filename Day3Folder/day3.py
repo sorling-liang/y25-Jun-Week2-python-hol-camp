479036c573ac9fe831348b532160190971894d7b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-11 02:28:05 +0000

    Jun 11, 2025, 2:28 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -15,6 +15,7 @@ print("I am " + str(yourage) + " this year.") # convert number to string to prin
 yourname = input("what is your name?")
 yourtitle = input("what is your title?")
 yourcommand = input("what is your command?")
+p
 
 ########################################################################
 # Task 2:
